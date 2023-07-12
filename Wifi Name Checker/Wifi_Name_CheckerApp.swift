1272094b8b7a7ade28a699d0b10803841c89800f
Giorgio Giannotta <jorge_upa@msn.com>
2023-07-12 07:10:05 +0200

    Initial Commit

---
+++ Wifi Name Checker/Wifi_Name_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wifi_Name_CheckerApp.swift
+//  Wifi Name Checker
+//
+//  Created by Giorgio Giannotta on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Wifi_Name_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
